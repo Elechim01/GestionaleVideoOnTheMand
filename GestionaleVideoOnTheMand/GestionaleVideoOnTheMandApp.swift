d221080ac6784d28d65eb712d2f90675da8bc1fc
Elechim01 <68447158+Elechim01@users.noreply.github.com>
2022-08-11 14:09:18 +0200

    Initial Commit

---
+++ GestionaleVideoOnTheMand/GestionaleVideoOnTheMandApp.swift
@@ -0,0 +1,17 @@
+//
+//  GestionaleVideoOnTheMandApp.swift
+//  GestionaleVideoOnTheMand
+//
+//  Created by Michele Manniello on 11/08/22.
+//
+
+import SwiftUI
+
+@main
+struct GestionaleVideoOnTheMandApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
